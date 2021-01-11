3d8d3b4ebef7eb0e1a709194396785083a72c278
Jonathan Gonzalez <jonathangc.awt@gmail.com>
2021-01-11 18:45:34 -0500

    la segunda opcion nos ofrece un tunel de capa para conexion de grupos

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -1,23 +1,72 @@
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
 
-    def disconnect(self, close_code):
-        pass
+'''##Esta arte funciona muy bien sin docker
+    class ChatConsumer(WebsocketConsumer):
+        def connect(self):
+            self.accept()
 
-    def receive(self, text_data):
-        try:
+        def disconnect(self, close_code):
+            pass
+
+        def receive(self, text_data):
+            try:
+                text_data_json = json.loads(text_data)
+                message = text_data_json["message"]
+                print(f"> {message}")
+                texto2 = json.dumps({"message": message })
+                self.send(texto2)
+            except:
+                text_data_json = json.loads(text_data)
+                id_tag = text_data_json[1]
+
+                texto2 = json.dumps([3, id_tag , {"currentTime": "2019-06-16T11:18:09.591716", "interval": 10, "status": "Accepted"}])
+                self.send(texto2)
+'''
+
+'''##Esta arte funciona muy bien con docker
+    class ChatConsumer(WebsocketConsumer):
+        def connect(self):
+            self.room_name = self.scope['url_route']['kwargs']['room_name']
+            self.room_group_name = 'chat_%s' % self.room_name
+
+            # Join room group
+            async_to_sync(self.channel_layer.group_add)(
+                self.room_group_name,
+                self.channel_name
+            )
+
+            self.accept()
+
+        def disconnect(self, close_code):
+            # Leave room group
+            async_to_sync(self.channel_layer.group_discard)(
+                self.room_group_name,
+                self.channel_name
+            )
+
+        # Receive message from WebSocket
+        def receive(self, text_data):
             text_data_json = json.loads(text_data)
-            message = text_data_json["message"]
-            print(f"> {message}")
-            texto2 = json.dumps({"message": message })
-            self.send(texto2)
-        except:
-            text_data_json = json.loads(text_data)
-            id_tag = text_data_json[1]
+            message = text_data_json['message']
 
-            texto2 = json.dumps([3, id_tag , {"currentTime": "2019-06-16T11:18:09.591716", "interval": 10, "status": "Accepted"}])
-            self.send(texto2)
+            # Send message to room group
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message
+                }
+            )
+
+        # Receive message from room group
+        def chat_message(self, event):
+            message = event['message']
+
+            # Send message to WebSocket
+            self.send(text_data=json.dumps({
+                'message': message
+            }))
+'''
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 ]
 
 #WSGI_APPLICATION = 'mysite.wsgi.application'
-ASGI_APPLICATION = "mysite.asgi.application"
+
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
@@ -120,3 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels
+ASGI_APPLICATION = "mysite.asgi.application"
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+
+}
